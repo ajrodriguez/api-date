4bcd86bfd7c30f0b4519420d6a0921a3cbf986b1
Antonio J. Rodríguez <arodriguezrios@gmail.com>
2018-04-24 20:38:46 +0200

    Added a part of OCA

--- src/main/java/com/ajrodriguez/oca/api/date/Main.java
+++ src/main/java/com/ajrodriguez/oca/api/date/Main.java
@@ -1,11 +1,22 @@
 package com.ajrodriguez.oca.api.date;
 
+import java.time.Clock;
 import java.time.DayOfWeek;
+import java.time.Duration;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
-import java.time.temporal.Temporal;
+import java.time.OffsetDateTime;
+import java.time.Period;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjuster;
 import java.time.temporal.TemporalAdjusters;
 
 public class Main {
@@ -44,10 +55,49 @@ public class Main {
 		System.out.println(localDandT);
 		
 		// Instant (from 1970/01/01)
+		Instant instant = Instant.ofEpochSecond(120);
+		Instant epoch = Instant.EPOCH;
+		Instant.now();
+		Instant.now(new Clock() {
+			
+			@Override
+			public Clock withZone(ZoneId zone) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public Instant instant() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public ZoneId getZone() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+		});
 		
 		// Duration & Period
+		LocalTime localTime01 = LocalTime.of(12, 25);
+		LocalTime localTime02 = LocalTime.of(17, 35);
+		Duration duration = Duration.between(localTime01, localTime02);
 		
-		// Temporal
+		Duration oneDayDuration1 = Duration.of(1, ChronoUnit.DAYS);
+		Duration oneDayDuration2 = Duration.ofDays(1);
+		
+		LocalDate localDate01 = LocalDate.of(2016, Month.JULY, 18);
+		LocalDate localDate02 = LocalDate.of(2016, Month.JULY, 20);
+		Period period = Period.between(localDate01, localDate02);
+		
+		Period period01 = Period.of(1, 2, 3);
+		
+		Period period02 = Period.ofYears(1);
+		
+		
+		// TemporalAdjusters
+		// https://docs.oracle.com/javase/8/docs/api/java/time/temporal/TemporalAdjusters.html
 		LocalDateTime localDateAndTime = LocalDateTime.now();
 		
 		LocalDateTime dateTime1 = localDateAndTime.with(TemporalAdjusters.next(DayOfWeek.SUNDAY));
@@ -57,6 +107,26 @@ public class Main {
 		System.out.println(dateTime1);
 		System.out.println(dateTime2);
 		System.out.println(dateTime3);
+		
+		TemporalAdjuster TWO_DAYS_LATER =
+			       TemporalAdjusters.ofDateAdjuster(date -> date.plusDays(2));
+		
+		// Temporal
+		// https://docs.oracle.com/javase/8/docs/api/java/time/temporal/Temporal.html
+		
+		// Formatters
+		LocalDateTime dateTime = LocalDateTime.of(2016, Month.JULY, 25, 15, 30);
+		OffsetDateTime offsetDateTime = dateTime.atOffset(ZoneOffset.ofHours(-5));
+		DateTimeFormatter formatter = new DateTimeFormatterBuilder()
+		                                  .appendText(ChronoField.DAY_OF_MONTH)
+		                                  .appendLiteral(" ")
+		                                  .appendText(ChronoField.MONTH_OF_YEAR)
+		                                  .appendLiteral(" ")
+		                                  .appendText(ChronoField.YEAR)
+		                                  .appendOffsetId()
+		                                  .toFormatter();
+		offsetDateTime.format(formatter);
+		formatter.format(offsetDateTime);
 		
 	}
 
